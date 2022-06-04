4d826c5cccb62b05d25b8b530df625bb7493a253
Ruthvik L <48211227+ruthvikl@users.noreply.github.com>
2022-06-04 15:12:11 +0530

    firebase hosting

---
+++ build/precache-manifest.fa10dbbc2fdb53ba2ed205fe46ee1512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0d11f7d759a68b9aa0627020961b6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28741e11a1348c2f1296",
+    "url": "/static/css/main.33b6b74d.chunk.css"
+  },
+  {
+    "revision": "15a2fe9ad89c592aaf6f",
+    "url": "/static/js/2.68978b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.68978b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28741e11a1348c2f1296",
+    "url": "/static/js/main.eb9b9c11.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
